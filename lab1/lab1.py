7ec096b78b697a8db55600b160188771dd7021b1
Dany <89897989+honeyittsya@users.noreply.github.com>
2021-10-03 20:12:47 +0300

    Create lab1.py

---
+++ lab1/lab1.py
@@ -0,0 +1,8 @@
+def solve():
+        a = float(input("First cathed: "))
+        b = float(input("Second cathed: "))
+        sq = (a*b)/2
+        return sq
+
+if __name__ == "__main__":
+        print("Square of triangle is : ", round(solve(), 2))
